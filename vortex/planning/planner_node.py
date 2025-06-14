2b4ddf33d08414a6488876aaa7a3eae0e9215a90
manohara <manohara01012005@gmail.com>
2025-06-14 19:54:38 +0530

    velocity publisher

--- vortex/planning/planner_node.py
+++ vortex/planning/planner_node.py
@@ -1,14 +1,23 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 
-class MyNode(Node):
+class PlannerNode(Node):
     def __init__(self):
         super().__init__("planner_node")
-        self.get_logger().info("hello")
+        self.get_logger().info("Planner node has started autonomous navigation")
+        self.cmd_vel_pub_ = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.timer = self.create_timer(0.5, self.send_velocity_msg)
+
+    def send_velocity_msg(self):
+        msg = Twist()
+        msg.linear.x = 2.0  
+        msg.angular.z = 0.0
+        self.cmd_vel_pub_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyNode()
+    node = PlannerNode()
 
     try:
         rclpy.spin(node)
@@ -16,9 +25,10 @@ def main(args=None):
         print(f"{node.get_name()} received Ctrl+C, shutting down...")
     finally:
         if rclpy.ok():
+            node_name = node.get_name() 
             node.destroy_node()
             rclpy.shutdown()
-        print(f"{node.get_name()} shutdown complete.")
+            print(f"{node_name} shutdown complete.")
 
 if __name__ == '__main__':
     main()
